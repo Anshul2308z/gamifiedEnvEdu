cd5d73c52112ea2e05b137dcf4ea61ca8cc7d982
Anshul <nemaanshul42@gmail.com>
2025-09-04 09:26:58 +0530

    landing page -aditya

--- client/src/pages/Landing.jsx
+++ client/src/pages/Landing.jsx
@@ -6,7 +6,16 @@ export default function Landing(){
         <Navbar/>
         {/* stuff on the landing page */}
         <div className="italic"> hello</div>  {/*check*/}
+        
+        <div> Hello ! Welcome to the new world of learning. </div> {/*Greeting*/}
+        <section> 
+            This is a platform which will help you to open new ways of learning of Enviourmental Studies. {/*About Section*/}
+        </section>
+        <section>
+            Start Yout Journey by Clicking the Button { /*Add some features accordingly*/}
+            <button> Join Now! </button>
+        </section>
+
         </>
     )
 }
-
--- client/src/pages/Login.jsx
+++ client/src/pages/Login.jsx
@@ -1,9 +1,9 @@
 import {Link} from "react-router-dom"
 
-export default function Login(){
+ export default function(){
   return (
-  <>
-  <h3>login...</h3>
-  <div>new here, <Link to="/signup">signup</Link></div>
-  </>)
-}
+    <div>login page
+      
+    </div>
+  )
+ }
